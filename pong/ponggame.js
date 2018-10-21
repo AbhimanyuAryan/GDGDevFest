14e554efad186e4347054eef51faf66c46abefe3
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-10-21 16:58:53 +0530

    more pong game code

--- pong/ponggame.js
+++ pong/ponggame.js
@@ -122,3 +122,84 @@ Computer.prototype.update = function (ball) {
 Computer.prototype.ai_update = function (move = 0) {
     this.paddle.move(4 * move, 0);
 };
+
+
+//from pong code:
+function Player() {
+    this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+//from pong code:
+Player.prototype.render = function () {
+    this.paddle.render();
+};
+
+//from pong code:
+Player.prototype.update = function () {
+    for (var key in keysDown) {
+        var value = Number(key);
+        if (value == 37) {
+            this.paddle.move(-4, 0);
+        } else if (value == 39) {
+            this.paddle.move(4, 0);
+        } else {
+            this.paddle.move(0, 0);
+        }
+    }
+};
+
+//from pong code:
+function Ball(x, y) {
+    this.x = x;
+    this.y = y;
+    this.x_speed = 0;
+    this.y_speed = 3;
+}
+
+//from pong code:
+Ball.prototype.render = function () {
+    context.beginPath();
+    context.arc(this.x, this.y, 5, 2 * Math.PI, false);
+    context.fillStyle = "#ddff59";
+    context.fill();
+};
+
+//from pong code:
+Ball.prototype.update = function (paddle1, paddle2, new_turn) {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.x - 5;
+    var top_y = this.y - 5;
+    var bottom_x = this.x + 5;
+    var bottom_y = this.y + 5;
+
+    if (this.x - 5 < 0) {
+        this.x = 5;
+        this.x_speed = -this.x_speed;
+    } else if (this.x + 5 > 400) {
+        this.x = 395;
+        this.x_speed = -this.x_speed;
+    }
+
+    if (this.y < 0 || this.y > 600) {
+        this.x_speed = 0;
+        this.y_speed = 3;
+        this.x = 200;
+        this.y = 300;
+        ai.new_turn();
+    }
+
+    if (top_y > 300) {
+        if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+            this.y_speed = -3;
+            this.x_speed += (paddle1.x_speed / 2);
+            this.y += this.y_speed;
+        }
+    } else {
+        if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+            this.y_speed = 3;
+            this.x_speed += (paddle2.x_speed / 2);
+            this.y += this.y_speed;
+        }
+    }
+};
